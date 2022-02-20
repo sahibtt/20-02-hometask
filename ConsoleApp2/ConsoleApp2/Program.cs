6ed72e3201ac68eac9a346c05067f3df84bb08f1
sahibtt <sahibtt@code.edu.az>
2022-02-20 07:09:15 -0800

    first

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User u = new User();
+            Console.Write("Username'i daxil edin: ");
+            u.UserName = Console.ReadLine();
+            Console.Write("Password'u daxil edin: ");
+            u.Password = Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class User
+    {
+        private string _username;
+        private string _password;
+        public User()
+        {
+            Console.WriteLine("Sisteme Xosh gelmishsiniz!");
+        }
+
+        public string UserName
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                if (value.Length < 8)
+                {
+                    throw new Exception("Minimum 8 simvoldan ibaret olmalidir!");
+                }
+                value = _username;
+
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (value.Length < 8)
+                {
+                    throw new Exception("Minimum 8 simvoldan ibaret olmalidir!");
+                }
+                value = _password;
+            }
+        }
+
+    }
+}
