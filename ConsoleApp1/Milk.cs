a75926216217d0306bb173c35ab82685df4fcd35
sahibtt <sahibtt@code.edu.az>
2022-02-20 07:19:28 -0800

    firstt

---
+++ ConsoleApp1/Milk.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Milk : Product
+    {
+        public int Volume { get; set; }
+        public int FatRate { get; set; }
+    }
+}
---
+++ ConsoleApp1/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Count { get; set; }
+        public int TotalIncome { get; set; }
+
+        public void Sell()
+        {
+            if (Count == 0)
+            {
+                return;
+            }
+            Count = Count - 1;
+            TotalIncome = TotalIncome + Price;
+        }
+
+    }
+}
